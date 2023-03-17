cbf546b19fb360b9857fbe63558da37c0f01efe7
MarkUnderwood-DigtialPlanning <76440778+MarkUnderwood-DigtialPlanning@users.noreply.github.com>
2023-03-17 08:45:08 +0000

    TestApp2 updates

---
+++ TestTestApp2/Services/MockFinancialService.cs
@@ -0,0 +1,10 @@
+namespace TestApp2.Financial
+{
+    public class MockFinancialService : IFinancialService
+    {
+        public Task<List<BoxOfficeRevenueType>> GetBoxOfficeRevenue()
+        {
+            return Task.FromResult<List<BoxOfficeRevenueType>>(new());
+        }
+    }
+}
